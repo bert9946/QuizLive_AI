604089163b3a27a36923f25e09df1cd8116c675e
bert9946 <53603989+bert9946@users.noreply.github.com>
2024-02-23 18:18:50 +0800

    Update GPT model in gpt.py

--- gpt.py
+++ gpt.py
@@ -3,8 +3,7 @@ from openai import OpenAI
 def Anwser(text):
   client = OpenAI()
   completion = client.chat.completions.create(
-    # model="gpt-3.5-turbo-0125",
-    model="ft:gpt-3.5-turbo-1106:personal::8tFf4XWJ",
+    model="gpt-3.5-turbo-0125",
     messages=[
       {"role": "system", "content": "根據問題，回答最可能的答案。（只要回答選項的數字順序以及文字，如：「1. 選項一」。）"},
       {"role": "user", "content": text}
