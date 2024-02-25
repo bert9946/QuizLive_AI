c49914fec190ec99d0c0a5e5e4f2a30cf8c3ff19
bert9946 <53603989+bert9946@users.noreply.github.com>
2024-02-25 17:04:39 +0800

    Refactor util.py: Fix indentation

--- src/util.py
+++ src/util.py
@@ -6,13 +6,13 @@ import cv2 as cv
 from adb import simulateTap
 
 def getWindowId():
-	command = ['osascript', '-e', 'tell app "QuickTime Player" to id of window 1']
-	process = subprocess.run(command, capture_output=True, text=True)
-	return int(process.stdout)
+    command = ['osascript', '-e', 'tell app "QuickTime Player" to id of window 1']
+    process = subprocess.run(command, capture_output=True, text=True)
+    return int(process.stdout)
 
 def captureWindow(image_path='tmp/test.jpg', window_id=None):
-	capture_command = ['screencapture', '-o', f'-l{window_id}', image_path]
-	subprocess.run(capture_command)
+    capture_command = ['screencapture', '-o', f'-l{window_id}', image_path]
+    subprocess.run(capture_command)
 
 def calculateExecutionTime(start_time, end_time):
     return int((end_time - start_time) * 1000)
@@ -42,24 +42,24 @@ def json_to_jsonl(input_path, output_path):
 
 # from log data to fine_tuning data
 def modifyData(data):
-	modified_data = {
-		'messages':
-			[
-				{
-					'role': 'system',
-					'content': '根據問題，回答最可能的答案。（只要回答選項的數字順序以及文字，如：「1. 選項一」。）'
-				},
-				{
-					'role': 'user',
-					'content': data['question'] + '\n' + '\n'.join(data['options'])
-				},
-				{
-					'role': 'assistant',
-					'content': data['real_ans']+'. '+data['options'][int(data['real_ans'])-1]
-				}
-			]
-	}
-	return modified_data
+    modified_data = {
+        'messages':
+            [
+                {
+                    'role': 'system',
+                    'content': '根據問題，回答最可能的答案。（只要回答選項的數字順序以及文字，如：「1. 選項一」。）'
+                },
+                {
+                    'role': 'user',
+                    'content': data['question'] + '\n' + '\n'.join(data['options'])
+                },
+                {
+                    'role': 'assistant',
+                    'content': data['real_ans']+'. '+data['options'][int(data['real_ans'])-1]
+                }
+            ]
+    }
+    return modified_data
 
 def speak(text, rate=225):
     command = ['say', text, '--rate', str(rate)]
