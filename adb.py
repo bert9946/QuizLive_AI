fc8f5d9756f856e53e85dc3bbc89367604738c2a
bert9946 <53603989+bert9946@users.noreply.github.com>
2024-02-29 21:35:31 +0800

    Remove commented out code and unused imports in adb.py

--- adb.py
+++ adb.py
@@ -3,8 +3,6 @@ import subprocess
 def adb(command):
 	return subprocess.run(['/Users/bert9946/Library/Android/sdk/platform-tools/adb'] + command, capture_output=True, text=True).stdout
 
-# command = ['shell', 'input', 'tap', '1000', '2700']
-
 def simulateTap(x, y):
 	command = ['shell', 'input', 'tap', str(x), str(y)]
 	adb(command)
@@ -17,10 +15,3 @@ def tapOption(option_index):
 	delta_y = 330
 
 	simulateTap(x, y + delta_y * (option_index - 1))
-
-# tap('1') # tap the first option
-# simulateTap(1000, 2700) # tap the middle of the screen
-
-# while True:
-# 	option = input('Press Enter to tap: ')
-# 	tap(option) # tap the first option
