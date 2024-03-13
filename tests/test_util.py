4b7b24f38b7a47fc8ccc9823a044fb57f57bd643
bert9946 <53603989+bert9946@users.noreply.github.com>
2024-03-13 10:51:00 +0800

    Refactor text in test_splitQuestionAndOptions function

--- tests/test_util.py
+++ tests/test_util.py
@@ -7,28 +7,18 @@ def test_calculateExecutionTime():
 	assert result == 1000
 
 def test_splitQuestionAndOptions():
-	text1 = '''常見的人造纖維
-「Polyester」的中文是？
-酚醛樹脂
-聚異丁烯
-聚醯亞胺
-聚酯纖維
-'''
-
-	text2 = '''這個問題
-沒有問號
-選項一
-選項二
-選項三
-選項四'''
+	text1 = (
+		'常見的人造纖維\n'
+		'「Polyester」的中文是？\n'
+		'酚醛樹脂\n'
+		'聚異丁烯\n'
+		'聚醯亞胺\n'
+		'聚酯纖維\n'
+	)
 
 	question1, options1 = splitQuestionAndOptions(text1)
 	assert question1 == '常見的人造纖維「Polyester」的中文是？'
 	assert options1 == ['酚醛樹脂', '聚異丁烯', '聚醯亞胺', '聚酯纖維']
-
-	# question2, options2 = splitQuestionAndOptions(text2)
-	# assert question2 == '這個問題沒有問號'
-	# assert options2 == ['選項一', '選項二', '選項三', '選項四']
 
 def test_matchOption():
 	text = 'c'
